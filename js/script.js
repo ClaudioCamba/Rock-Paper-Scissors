d8d427992fe768f55e981f50e3f263d74c482a8a
Claudio Camba <claudio.a.jose@gmail.com>
2022-04-11 18:06:31 +0100

    Add button variable refrence and register player input from button data-pick

--- js/script.js
+++ js/script.js
@@ -1,64 +1,72 @@
-// Stored const/let variables 
-const optionsRPC = ['rock', 'paper', 'scissors'],
-    logicRPC = { 'rock': 'scissors', 'paper': 'rock', 'scissors': 'paper' };
+// Stored const variables 
+const optionsRPC = ['rock', 'paper', 'scissors'];
+const logicRPC = { 'rock': 'scissors', 'paper': 'rock', 'scissors': 'paper' };
+const playButtons = document.querySelectorAll('.buttons-wrap button');
 
 // Global variables to store
 let playerSelection,
-    computerSelection,
     gameTracker = {
         'Player': 0,
         'Computer': 0
     };
 
-//  Function - Computer randomly select option 
+// Function - Computer randomly select option 
 function computerPlay() {
-    randomRPC = optionsRPC[Math.floor(Math.random() * optionsRPC.length)];
+    let randomRPC = optionsRPC[Math.floor(Math.random() * optionsRPC.length)];
+    // return randomRPC;
     return randomRPC;
 }
 
-// Function - Play a single round
-function playRound(playerSelection, computerSelection) {
+// Function - Get player option and play round
+function playerChoice(e) {
+    playerSelection = this.getAttribute('data-pick');
+    playRound(playerSelection, computerPlay())
+}
 
-    // Get player / computer options
-    playerSelection = prompt().toLowerCase();
-    computerSelection = computerPlay();
+
+// Function - Play a single round
+function playRound(player, comp) {
 
     // Option comparison logic
-    if (logicRPC[playerSelection] === computerSelection) { // If player option beats computer option 
-        return 'Player Win! Player: ' + playerSelection + ' beats Computer: ' + computerSelection;
-    } else if (playerSelection !== computerSelection) { // If computer option beats player option 
-        return 'Computer Win! Computer: ' + computerSelection + ' beats Player: ' + playerSelection;
+    if (logicRPC[player] === comp) { // If player option beats computer option 
+        // return 'Player Win! Player: ' + player + ' beats Computer: ' + comp;
+        console.log('Player Win! Player: ' + player + ' beats Computer: ' + comp);
+    } else if (player !== comp) { // If computer option beats player option 
+        // return 'Computer Win! Computer: ' + comp + ' beats Player: ' + player;
+        console.log('Computer Win! Computer: ' + comp + ' beats Player: ' + player);
     } else {
-        return 'Draw! Player: ' + computerSelection + ' & Computer: ' + playerSelection + ' are equal';
+        // return 'Draw! Player: ' + comp + ' & Computer: ' + player + ' are equal';
+        console.log('Draw! Player: ' + comp + ' & Computer: ' + player + ' are equal');
     }
 
 }
 
-// Function to track score and console.log round and game winner/loser
-function game() {
-    // Reset scores for new game
-    gameTracker.Player = 0;
-    gameTracker.Computer = 0;
-    // Loop playRound function to play multiple times
-    for (let i = 0; i < 5; i++) {
-        playRoundScore = playRound();
-        if (playRoundScore.indexOf('Player Win') > -1) {
-            gameTracker.Player++;
-        } else if (playRoundScore.indexOf('Computer Win') > -1) {
-            gameTracker.Computer++;
-        }
-        console.log('ROUND[' + (i + 1) + '] | ' + playRoundScore);
-    }
-    // Final console.log for the winner/loser
-    if (gameTracker.Player > gameTracker.Computer) {
-        console.log('YOU WIN!! - Player ' + gameTracker.Player + ' | Computer: ' + gameTracker.Computer);
-    } else if (gameTracker.Player < gameTracker.Computer) {
-        console.log('YOU LOSE!! - Player ' + gameTracker.Player + ' | Computer: ' + gameTracker.Computer);
-    } else {
-        console.log('ITS A DRAW!! - Player ' + gameTracker.Player + ' | Computer: ' + gameTracker.Computer);
-    }
-}
+// // Function to track score and console.log round and game winner/loser
+// function game() {
+//     // Reset scores for new game
+//     gameTracker.Player = 0;
+//     gameTracker.Computer = 0;
+//     // Loop playRound function to play multiple times
+//     for (let i = 0; i < 5; i++) {
+//         playRoundScore = playRound();
+//         if (playRoundScore.indexOf('Player Win') > -1) {
+//             gameTracker.Player++;
+//         } else if (playRoundScore.indexOf('Computer Win') > -1) {
+//             gameTracker.Computer++;
+//         }
+//         console.log('ROUND[' + (i + 1) + '] | ' + playRoundScore);
+//     }
+//     // Final console.log for the winner/loser
+//     if (gameTracker.Player > gameTracker.Computer) {
+//         console.log('YOU WIN!! - Player ' + gameTracker.Player + ' | Computer: ' + gameTracker.Computer);
+//     } else if (gameTracker.Player < gameTracker.Computer) {
+//         console.log('YOU LOSE!! - Player ' + gameTracker.Player + ' | Computer: ' + gameTracker.Computer);
+//     } else {
+//         console.log('ITS A DRAW!! - Player ' + gameTracker.Player + ' | Computer: ' + gameTracker.Computer);
+//     }
+// }
 
-// Run the game 
+// Run the game
 // game();
 
+playButtons.forEach(playButtons => playButtons.addEventListener('click', playerChoice));
